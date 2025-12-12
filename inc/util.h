0f56b346d4095b03fbb765b51721fe93c5ac194c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-12 18:16:06 +0000

    added wip utilities
    added these bit shift operators for now, to help with accessing general purpose info

---
+++ inc/util.h
@@ -0,0 +1,35 @@
+// COPYRIGHT (C) HARRY CLARK 2025
+// HITACHI SUPERH INSTRUCTION DECODER
+
+// THE FOLLOWING FILE PERTAINS TOWARDS THE FUNCTIONALITY ENCOMPASSING
+// UTILIES FOR VARIOUS BIT OPERATIONS AND ANYTHING OF THE ASSOCIATION
+
+#ifndef CATH_UTIL_H
+#define CATH_UTIL_H
+
+// NESTED INCLUDES
+
+#include <common.h>
+
+// SYSTEM INCLUDES
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    // CREATE THE BASIS FOR GENERAL PURPOSE BIT SHIFT OPERATIONS
+    // 
+    // FOR SHIFT_L - LOWER BITS ARE MASKED, THEN SHIFTED
+    // FOR SHIFT_R - VALUE IS SHIFTED, THEN LOWER BITS ARE MASKED
+
+    #define         CATH_MASK(VALUE, WORD)                      ((VALUE) & ((1U << (WORD)) - 1U))
+    #define         CATH_SHIFT_L(VALUE, SHIFT, WORD)            (MASK((VALUE), (WORD)) <<  (SHIFT))
+    #define         CATH_SHIFT_R(VALUE, SHIFT, WORD)            (MASK((VALUE) >> (SHIFT), (WORD)))
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
