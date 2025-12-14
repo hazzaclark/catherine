65b5f0683fe454823718bb8246be7eebe10e339c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-14 17:34:23 +0000

    Update descriptors.c

--- inc/tables/descriptors.c
+++ inc/tables/descriptors.c
@@ -11,8 +11,27 @@ const SH_DESCRIPTOR INSTR_DESCRIPTORS[] =
 {
     [CATH_INSTR_ID_INVALID] = 
     {
-        .OPERAND_TYPE={OPERAND_NONE}, .IS_BRANCH = false, 
+        .OPERAND_TYPE = {OPERAND_NONE}, .IS_BRANCH = false, 
         .IS_JUMP = false, .IS_JUMP_ADDRESS = false,
         .IS_TRAP = false, .IS_HALT = false,
-        .IS_FLOAT = false, .IS_UNSIGNED = false}
+        .IS_FLOAT = false, .IS_UNSIGNED = false
+    },
+
+    [CATH_INSTR_ID_ADD] = 
+    {
+        .OPERAND_TYPE = {OPERAND_RM, OPERAND_RN, OPERAND_NONE},
+        .IS_BRANCH = false,.IS_JUMP = false,
+        .IS_JUMP_ADDRESS = false, .IS_TRAP = false,
+        .IS_HALT = false, .HAS_DELAY_SLOT = false,
+        .IS_FLOAT = false, .IS_UNSIGNED = false,
+    },
+
+    [CATH_INSTR_ID_ADDI] =
+    {
+        .OPERAND_TYPE = {OPERAND_IMM, OPERAND_RN, OPERAND_NONE},
+        .IS_BRANCH = false, .IS_JUMP = false,
+        .IS_JUMP_ADDRESS = false, .IS_TRAP = false,
+        .IS_HALT = false, .HAS_DELAY_SLOT = false,
+        .IS_FLOAT = false, .IS_UNSIGNED = false,
+    },
 };
