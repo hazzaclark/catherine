c7fca91c2435a87d8c044222b5f95d932aa9eda1
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-14 23:54:49 +0000

    added separate definers for register alias
    helps with accessing both variants of register types within Assembly

--- inc/instructions/register.h
+++ inc/instructions/register.h
@@ -8,25 +8,56 @@
 #ifndef CATH_REGISTER_H
 #define CATH_REGISTER_H
 
+#if defined(USE_CATH_REG)
+    #define USE_CATH_REG
+#else
+    #define USE_CATH_REG
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// NESTED INCLUDES  
+    // NESTED INCLUDES  
 
-#include "util.h"
-#include "instructions/instruction.h"
+    #include "util.h"
+    #include "instructions/instruction.h"
 
-#include "enums/register.h"
+    #include "enums/register.h"
 
- // DEFINE THE BASIS FOR THE LABELS ENCOMPASSING ALL REGISTER TYPES
-extern const char* CATH_GPR_NAMES[SH2_REG_MAX][2];
-extern const char* CATH_CTRL_NAMES[SH2_REG_CTRL_MAX];
-extern const char* CATH_SYS_NAMES[SH2_REG_SYS_MAX];
+    // DEFINE THE BASIS FOR THE LABELS ENCOMPASSING ALL REGISTER TYPES
+    extern const char* CATH_GPR_NAMES[SH2_REG_MAX][2];
+    extern const char* CATH_CTRL_NAMES[SH2_REG_CTRL_MAX];
+    extern const char* CATH_SYS_NAMES[SH2_REG_SYS_MAX];
 
+    // DEFINE THE CHARACTERISTICS PER REGISTER TYPE
+    // TO BE ABLE TO READ PROPERLY FROM THEIR RESPECTIVE ENUM
+    typedef enum CATH_REG_ALIAS
+    {
+        CATH_REG_UPPER,
+        CATH_REG_LOWER,
+        
+    } CATH_REG_ALIAS;
+
+    // SIMPLE CONFIG TO HOUSE THE CHARACTERISTICS
+    // ENCOMPASSING REGISTER TYPES
+    typedef struct CATH_CONFIG
+    {
+        CATH_REG_ALIAS REG_ALIAS;
+        bool USE_ALIAS;
+
+    } CATH_CONFIG;
+
+    //////////////////////////////////////////
+    //         FUNCTION PROTOTYPES
+    //////////////////////////////////////////
+
+    const char* CATH_REGISTER_GET_GPRNAME(U8);
+    const char* CATH_REGISTER_GET_CTRLNAME(U8);
+    const char* CATH_REGISTER_GET_SYSNAME(U8);
 
 #ifdef __cplusplus
-#endif
 }
 
+#endif
+#endif
 #endif
