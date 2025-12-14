c36dd0df0c0d807a1a04dc2d5e803fff53409a9d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-14 01:08:51 +0000

    initial table commit
    focusing on disp scale for specified instructions

---
+++ inc/tables/tables.h
@@ -0,0 +1,22 @@
+// COPYRIGHT (C) HARRY CLARK 2025
+// HITACHI SUPERH INSTRUCTION DECODER
+
+#ifndef CATH_TABLES_H
+#define CATH_TABLES_H
+
+// NESTED INCLUDES  
+
+#include "common.h"
+#include "enums/instructions.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const U8 CATH_DISP_SCALE[CATH_INSTR_ID_ALL_MAX];
+
+#ifdef __cplusplus
+}
+
+#endif
+#endif
