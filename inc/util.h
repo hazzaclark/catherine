1fe68e3c22447425bb38fd6c189905412c15287e
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-15 13:16:30 +0000

    added buffer formatting for decoding

--- inc/util.h
+++ inc/util.h
@@ -23,11 +23,51 @@ extern "C" {
     // 
     // FOR SHIFT_L - LOWER BITS ARE MASKED, THEN SHIFTED
     // FOR SHIFT_R - VALUE IS SHIFTED, THEN LOWER BITS ARE MASKED
+    // BITFULL - MASKS AND SHIFTS THE FULL MANTISSA OF BITS WITHIN A SEQUENCE
 
     #define         CATH_MASK(VALUE, WORD)                      ((VALUE) & ((1U << (WORD)) - 1U))
     #define         CATH_SHIFT_L(VALUE, SHIFT, WORD)            (CATH_MASK((VALUE), (WORD)) <<  (SHIFT))
     #define         CATH_SHIFT_R(VALUE, SHIFT, WORD)            (CATH_MASK((VALUE) >> (SHIFT), (WORD)))
+    #define         CATH_BITFULL(FULL, VALUE, SHIFT, WORD)      ((CATH_SHIFT_R((FULL), (SHIFT) + (WORD), 32U - ((SHIFT) + (WORD))) | CATH_SHIFT_L((VALUE, (SHIFT), (WORD)) | CATH_MASK((FULL), (SHIFT)))))
 
+    // CREATES A DIRECTIVE TO HELP WITH PRE-PROCESSED 
+    // BUFFER SIZES AT COMPILE TIME
+    // 
+    // DETERMINES THE CURRENT SIZE AND ADDS TO
+    // THE TOTAL - HELPS WITH POSITIONS OF BUFFER READERS
+
+    #define         CATH_BUFFER(BUFFER, SIZE, EXP)              \
+        do                                                      \
+        {                                                       \
+            UNK TEMP_SIZE = (UNK)(EXP);                         \
+            if((BUFFER) != NULL)                                \
+            {                                                   \
+                (BUFFER) += TEMP_SIZE;                          \
+            }                                                   \
+            (SIZE) += TEMP_SIZE;                                \
+                                                                \
+        } while(0)
+
+
+    // DEFINE THE BASIS FOR BEING ABLE TO LEVERAGE 
+    // SPRINTF FOR AIDING IN STRING FORMATTING  
+
+    #define         CATH_SPRINTF(BUFFER, SIZE, FMT, ...)        \
+        do                                                      \  
+        {                                                       \
+            int LENGTH;                                         \
+            if((BUFFER) != NULL)                                \
+            {                                                   \
+                LENGTH = sprintf(BUFFER, FMT, __VA_ARGS__);     \
+            }                                                   \
+            else                                                \
+            {                                                   \
+                LENGTH = snprintf(NULL, 0, FMT, __VA_ARGS__);   \
+            }                                                   \
+                                                                \
+            CATH_BUFFER(BUFFER, SIZE, LENGTH);                  \
+                                                                \
+        } while(0)
 
 #ifdef __cplusplus
 }
