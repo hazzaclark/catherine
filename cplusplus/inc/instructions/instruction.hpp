305c1d20cf80cb60683c3d9fcec901028dcfc011
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-18 21:32:23 +0000

    added initial getters

--- cplusplus/inc/instructions/instruction.hpp
+++ cplusplus/inc/instructions/instruction.hpp
@@ -6,7 +6,7 @@
 // AND HELPER FUNCTIONS - ALL PERTAINING TOWARDS THE INSTRUCTIONS
 
 #ifndef CATH_INSTR_HPP
-#define  CATH_INSTR_HPP
+#define CATH_INSTR_HPP
 
 #pragma once
 
@@ -15,6 +15,10 @@
 #include "common.h"
 #include "util.h"
 #include "instructions/instruction.h"
+#include "instructions/operands.h"
+#include "enums/category.h"
+#include "enums/instructions.h"
+#include "enums/register.h"
 
 namespace catherine
 {
@@ -22,6 +26,22 @@ namespace catherine
     {
         protected:
             SH_INSTRUCTION INSTR;
+
+            CATH_INSTRUCTION() = default;
+            virtual ~CATH_INSTRUCTION() = default;
+        
+        public:
+            // THE FOLLOWING METHODS SERVE TO ACT AS
+            // GENERAL GETTERS AND SETTERS WHICH ACCESS
+            // THE API FEATURES AND JUST REWORK THEM 
+            // FOR THIS CONTEXT
+
+            U8 CATH_GET_OPCODE4() const;
+            U8 CATH_GET_OPCODE8() const;
+            U8 CATH_GET_OPCODE12() const;
+
+            U8 CATH_GET_RN() const;
+            U8 CATH_GET_RM() const;
     };
 }
 
