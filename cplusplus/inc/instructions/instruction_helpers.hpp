c590fa680139951d205066d6e6b3ed919e0ac223
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-23 00:50:35 +0000

    added constexpr's for setting for gpr's

--- cplusplus/inc/instructions/instruction_helpers.hpp
+++ cplusplus/inc/instructions/instruction_helpers.hpp
@@ -18,6 +18,8 @@
 
 namespace catherine
 {
+    #define         CATH_BITMASK_WORD           0xF
+
     class CATH_INSTRUCTION_HELPER : public CATH_INSTRUCTION
     {
         public:
@@ -25,6 +27,23 @@ namespace catherine
             inline U32 CATH_GET_RAW_PC() const { return CATH_GET_PC(&INSTR); }
             inline S32 CATH_GET_BRA_OFFSET() const { return CATH_GET_BRANCH_OFFSET(&INSTR); }
     };
+
+    namespace helpers
+    {
+        // ACCESS THE CONTENTS OF THE REGISTERS SAFELY THROUGH
+        // THEIR RESPECTIVE ACCESS METHOD
+        static constexpr U16 CATH_SET_RN(U16 WORD, U16 VALUE)
+        {
+            return (WORD & static_cast<U16>(~(CATH_BITMASK_WORD << 8))) 
+            | static_cast<U16>((VALUE & CATH_BITMASK_WORD) << 8);
+        }
+
+        static constexpr U16 CATH_SET_Rm(U16 WORD, U16 VALUE)
+        {
+            return (WORD & static_cast<U16>(~(CATH_BITMASK_WORD << 4))) 
+            | static_cast<U16>((VALUE & CATH_BITMASK_WORD) << 4);
+        }
+    }
 }
 
 #endif
