f98d0b1f3dd7813ad41bf136910c1e346faf6041
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-26 18:19:03 +0000

    Update descriptors.c

--- inc/tables/descriptors.c
+++ inc/tables/descriptors.c
@@ -473,21 +473,21 @@
         },
 
         [CATH_INSTR_ID_MOVBL4] = {
-            .OPERAND_TYPE = {OPERAND_AT_DISP_RM, OPERAND_RN, OPERAND_NONE},
+            .OPERAND_TYPE = {OPERAND_AT_DISP_RM, OPERAND_R0, OPERAND_NONE},
             .IS_BRANCH = false, .IS_JUMP = false, .IS_JUMP_ADDRESS = false,
             .IS_TRAP = false, .IS_HALT = false, .HAS_DELAY_SLOT = false,
             .IS_FLOAT = false, .IS_UNSIGNED = false,
         },
 
         [CATH_INSTR_ID_MOVWL4] = {
-            .OPERAND_TYPE = {OPERAND_AT_DISP_RM, OPERAND_RN, OPERAND_NONE},
+            .OPERAND_TYPE = {OPERAND_AT_DISP_RM, OPERAND_R0, OPERAND_NONE},
             .IS_BRANCH = false, .IS_JUMP = false, .IS_JUMP_ADDRESS = false,
             .IS_TRAP = false, .IS_HALT = false, .HAS_DELAY_SLOT = false,
             .IS_FLOAT = false, .IS_UNSIGNED = false,
         },
 
         [CATH_INSTR_ID_MOVLL4] = {
-            .OPERAND_TYPE = {OPERAND_AT_DISP_RN, OPERAND_RM, OPERAND_NONE},
+            .OPERAND_TYPE = {OPERAND_AT_DISP_RM, OPERAND_R0, OPERAND_NONE},
             .IS_BRANCH = false, .IS_JUMP = false, .IS_JUMP_ADDRESS = false,
             .IS_TRAP = false, .IS_HALT = false, .HAS_DELAY_SLOT = false,
             .IS_FLOAT = false, .IS_UNSIGNED = false,
@@ -576,6 +576,8 @@
             .IS_TRAP = false, .IS_HALT = false, .HAS_DELAY_SLOT = false,
             .IS_FLOAT = false, .IS_UNSIGNED = false,
         },
+
+        // TODO: FIGURE OUT WHY RN MAPS AS R0 FOR THESE @DISP, GBR'S
 
         [CATH_INSTR_ID_MOVBSG] = {
             .OPERAND_TYPE = {OPERAND_RN, OPERAND_AT_DISP_GBR, OPERAND_NONE},
