6a1faf4d37d2c9c7ae6811ceff179f13eab86e39
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-12-26 19:35:29 +0000

    testing exact PC match in source

--- src/main.c
+++ src/main.c
@@ -9,16 +9,17 @@
 int main(void)
 {
     printf("HARRY CLARK - HITACHI SUPERH INSTRUCTION DECODER\n");
-    U16 WORD[] = {0x74F8, 0x4710, 0x8FEA, 0x73F8, 0xD808, 0x6002, 0xE1FF, 0x2011, 0x3098, 0x401E};
-    U32 ADDRESS = 0x06000000;
+    U16 WORD[] = {0xDF1B};
+    U32 ADDRESS = 0x06005F7A;
     
     for(UNK INDEX = 0; INDEX < sizeof(WORD) / sizeof(WORD[0]); INDEX++)
     {
         SH_INSTRUCTION INSTR;
         char BUFFER[128];
-        
+                
         CATH_INSTRUCTION_INIT(&INSTR, WORD[INDEX], ADDRESS);
         CATH_INSTRUCTION_PROCESS(&INSTR);
+
         CATH_INSTRUCTION_DISASM(&INSTR, BUFFER);
         
         printf("0x%08X: 0x%04X -> %s\n", ADDRESS, WORD[INDEX], BUFFER);
