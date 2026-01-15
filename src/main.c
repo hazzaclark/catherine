34a51b2bc25814ad60bf323139b56a3fcb4c11d8
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2026-01-15 01:34:40 +0000

    sanity test against FUN_0607074E in NiGHTS disasm

--- src/main.c
+++ src/main.c
@@ -9,8 +9,9 @@
 int main(void)
 {
     printf("HARRY CLARK - HITACHI SUPERH INSTRUCTION DECODER\n");
-    U16 WORD[] = {0xDF1B};
-    U32 ADDRESS = 0x06005F7A;
+    U16 WORD[] = {0x2FE6, 0x2FD6, 0x6E43, 0x2FC6, 0x2FB6, 0x2FA6, 0x6A53,
+                    0x4F22, 0x7FFC, 0x60E2, 0x2008, 0x8F07, 0x9B20};
+    U32 ADDRESS = 0x0607074E;
     
     for(UNK INDEX = 0; INDEX < sizeof(WORD) / sizeof(WORD[0]); INDEX++)
     {
