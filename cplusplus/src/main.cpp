4dce3675c9dd749553991f8c8f49e8e670d6884d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2026-01-22 01:39:59 +0000

    making sure that inlines are still working
    had some issues elsewhere with how the inline's werent scraping the proper conditions - this has been resolved

--- cplusplus/src/main.cpp
+++ cplusplus/src/main.cpp
@@ -9,10 +9,11 @@ using namespace catherine;
 
 int main(void)
 {
-    U16 WORD = 0xDF1B;
-    U32 PC = 0x0600000;
+    std::printf("HARRY CLARK - HITACHI SUPERH INSTRUCTION DECODER\n");
+    U16 WORD = {0xA100};
+    U32 PC = 0x06000000;
 
     catherine::CATH_INSTRUCTION_SH2 INSTR(WORD, PC);
-    printf("0x%X: %s\n", WORD, INSTR.CATH_DISASSEMBLE_INSTR().c_str());
+    std::printf("0x%08X: 0x%X: %s\n", INSTR.CATH_GET_RAW_PC(),  WORD, INSTR.CATH_DISASSEMBLE_INSTR().c_str());
     return 0;
 }
