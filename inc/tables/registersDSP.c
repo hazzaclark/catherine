655fb459be0d9631edf1b7d3e25ab51b6a6d8b4c
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2026-05-10 13:39:57 +0000

    added table for D1 bus access

--- inc/tables/registersDSP.c
+++ inc/tables/registersDSP.c
@@ -31,10 +31,53 @@ const char* CATH_DSP_CT_NAMES[DSP_REG_CT_MAX][2] =
     [DSP_REG_CT3]   =   { "CT3", "ct3" },
 };
 
-const char* CATH_DSP_RA_NAMES[DSP_REG_RA_MAX] =
+const char* CATH_DSP_RA_NAMES[DSP_REG_RA_MAX][2] =
 {
-    [DSP_REG_RA0]   =   { "RAM0" },
-    [DSP_REG_RA1]   =   { "RAM1" },
-    [DSP_REG_RA2]   =   { "RAM2" },
-    [DSP_REG_RA3]   =   { "RAM3" },
+    [DSP_REG_RA0]   =   { "RAM0", "ram0" },
+    [DSP_REG_RA1]   =   { "RAM1", "ram1" },
+    [DSP_REG_RA2]   =   { "RAM2", "ram2" },
+    [DSP_REG_RA3]   =   { "RAM3", "ram3" },
+};
+
+// SPECIFIC FIELDS ARE OUTLINED AS NULL AS A 
+// RESULT OF THEM BEING UNUSED PATTERNS
+
+const char* CATH_DSP_D_DEST_NAMES[16] = 
+{
+    [0x0]   =   "MC0",
+    [0x1]   =   "MC1",
+    [0x2]   =   "MC2",
+    [0x3]   =   "MC3",
+    [0x4]   =   "RX",
+    [0x5]   =   "PL",
+    [0x6]   =   "RA0",
+    [0x7]   =   "WA0",
+    [0x8]   =   NULL,
+    [0x9]   =   NULL,
+    [0xA]   =   "LOP",
+    [0xB]   =   "TOP",
+    [0xC]   =   "CT0",
+    [0xD]   =   "CT1",
+    [0xE]   =   "CT2",
+    [0xF]   =   "CT3",
+};
+
+const char* CATH_DSP_D_SRC_NAMES[16] =
+{
+    [0x0]   =   "DATA RAM0",
+    [0x1]   =   "DATA RAM1",
+    [0x2]   =   "DATA RAM2",
+    [0x3]   =   "DATA RAM3",
+    [0x4]   =   "DATA RAM0,CT0++",
+    [0x5]   =   "DATA RAM1,CT1++",
+    [0x6]   =   "DATA RAM2,CT2++",
+    [0x7]   =   "DATA RAM3,CT3++",
+    [0x8]   =   NULL,
+    [0x9]   =   "ALU LOW",
+    [0xA]   =   "ALU HIGH",
+    [0xB]   =   NULL,
+    [0xC]   =   NULL,
+    [0xD]   =   NULL,
+    [0xE]   =   NULL,
+    [0xF]   =   NULL,
 };
