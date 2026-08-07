809a2ea614a8261ca3c69b2eaf8521e29894aa16
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2026-08-07 22:10:01 +0000

    an earnest mistake with CMP/EQ imm
    the descriptor was evaluating it to an RM value despite the encoding always being R0
    
    this took me by surprise as i could have sworn that all of the issues with descriptors had been ironed out 
    
    not a problem

--- inc/tables/descriptors.c
+++ inc/tables/descriptors.c
@@ -193,7 +193,7 @@
         },
 
         [CATH_INSTR_ID_CMPEQI] = {
-            .OPERAND_TYPE = {OPERAND_IMM, OPERAND_RM, OPERAND_NONE},
+            .OPERAND_TYPE = {OPERAND_IMM, OPERAND_R0, OPERAND_NONE},
             .IS_BRANCH = false, .IS_JUMP = false, .IS_JUMP_ADDRESS = false,
             .IS_TRAP = false, .IS_HALT = false, .HAS_DELAY_SLOT = false,
             .IS_FLOAT = false, .IS_UNSIGNED = false,
